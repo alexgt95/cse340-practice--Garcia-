9151aa75d84b9ff1de8c2b505f5bc723c0bb94c0
alexgt95 <40178272+alexgt95@users.noreply.github.com>
2026-09-23 20:21:37 -0400

    U01 Practice Repo URLs

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 // Import express using ESM syntax
 import express from 'express';
 
+// Define the port number the server will listen on
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const PORT = process.env.PORT || 3000;
+
 // Create an instance of an Express application
 const app = express();
 
 const name = process.env.NAME;
 
 // Define a route handler for the root URL ('/')
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`);
+    const title = 'Welcome Home';
+    res.render('home', { title });
 });
 
-app.get('/new', (req,res) => {
-    res.send('This is a new route!');
-})
+app.get('/about', (req, res) => {
+    const title = 'About Me';
+    res.render('about', { title });
+});
 
-// Define the port number the server will listen on
-const PORT = 3000;
+app.get('/products', (req, res) => {
+    const title = 'Our Products';
+    res.render('products', { title });
+});
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
